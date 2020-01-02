b8fba2e20365feabe4b97c33d041b77bee0446cb
zhengxinlei <xinlei3166@126.com>
2020-01-02 16:42:07 +0800

    first commit

---
+++ src/utils/assist.js
@@ -0,0 +1,54 @@
+// 由一个组件，向上找到最近的指定组件
+export function findComponentUpward (context, componentName) {
+  let parent = context.$parent;
+  let name = parent.$options.name;
+
+  while (parent && (!name || [componentName].indexOf(name) < 0)) {
+    parent = parent.$parent;
+    if (parent) name = parent.$options.name;
+  }
+  return parent;
+}
+
+// 由一个组件，向上找到所有的指定组件
+export function findComponentsUpward (context, componentName) {
+  let parents = [];
+  const parent = context.$parent;
+
+  if (parent) {
+    if (parent.$options.name === componentName) parents.push(parent);
+    return parents.concat(findComponentsUpward(parent, componentName));
+  } else {
+    return [];
+  }
+}
+
+// 由一个组件，向下找到最近的指定组件
+export function findComponentDownward (context, componentName) {
+  const childrens = context.$children;
+  let children = null;
+
+  if (childrens.length) {
+    for (const child of childrens) {
+      const name = child.$options.name;
+
+      if (name === componentName) {
+        children = child;
+        break;
+      } else {
+        children = findComponentDownward(child, componentName);
+        if (children) break;
+      }
+    }
+  }
+  return children;
+}
+
+// 由一个组件，向下找到所有指定的组件
+export function findComponentsDownward (context, componentName) {
+  return context.$children.reduce((components, child) => {
+    if (child.$options.name === componentName) components.push(child);
+    const foundChilds = findComponentsDownward(child, componentName);
+    return components.concat(foundChilds);
+  }, []);
+}
---
+++ src/views/MyForm.vue
@@ -0,0 +1,70 @@
+<template>
+  <div>
+    <Form ref="form" :model="form" :rules="rules">
+      <FormItem prop="a" label="a">
+        <Input v-model="form.a"/>
+      </FormItem>
+      <FormItem prop="b" label="b">
+        <Input v-model="form.b"/>
+      </FormItem>
+      <FormItem prop="c" label="c">
+        <Input v-model="form.c"/>
+      </FormItem>
+      <FormItem prop="d" label="d">
+        <Input v-model="form.d"/>
+      </FormItem>
+      <FormItem>
+        <button @click="handleSubmit">提交</button>
+      </FormItem>
+    </Form>
+  </div>
+</template>
+
+<script>
+  import Form from '../components/form/Form'
+  import FormItem from '../components/form/FormItem'
+  import Input from '../components/input/Input'
+  export default {
+    name: 'MyForm',
+    components: { FormItem, Form, Input },
+    data() {
+      return {
+        form: {
+          a: '',
+          b: '',
+          c: '',
+          d: ''
+        },
+        rules: {
+          a: [
+            { required: true, message: '请输入a', trigger: 'blur' }
+          ],
+          b: [
+            { required: true, message: '请输入b', trigger: 'blur' }
+          ],
+          c: [
+            { required: true, message: '请输入c', trigger: 'blur' }
+          ]
+        }
+      }
+    },
+    methods: {
+      handleSubmit (e) {
+        this.$refs.form.validate((valid) => {
+          if (valid) {
+            console.log('提交成功');
+            console.log(this.from)
+            e.preventDefault()
+          } else {
+            console.log('表单校验失败');
+            e.preventDefault()
+          }
+        })
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
